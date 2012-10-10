79a33ace81d986e4c05c70b631680e8e779fea96
omoon <omooon@gmail.com>
2012-10-10 16:13:21 +0900

    title.

--- fuel/app/views/welcome/index.php
+++ fuel/app/views/welcome/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Instagramen</title>
+	<title><?php echo Input::server('APP_NAME'); ?></title>
 	<?php echo Asset::css('bootstrap.css'); ?>
 	<style>
 		#logo{
