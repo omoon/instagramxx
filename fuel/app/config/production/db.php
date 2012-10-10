c3dbf1914dc8b200b59182b73de319f3c808b00c
Sotaro OMURA <omooon@gmail.com>
2012-10-10 21:01:28 +0900

    database settings.

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -2,13 +2,15 @@
 /**
  * The production database settings.
  */
-
 return array(
-	'default' => array(
-		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_prod',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
-		),
-	),
+    'default' => array(
+        'connection'  => array(
+            'dsn'        => 'mysql:host='.$_SERVER['DB1_HOST'].';port='.$_SERVER['DB1_PORT'].';dbname='.$_SERVER['DB1_NAME'],
+            'username'   => $_SERVER['DB1_USER'],
+            'password'   => $_SERVER['DB1_PASS'],
+            'persistent' => false,
+            'compress'   => false,
+        ),
+    ),
 );
+
